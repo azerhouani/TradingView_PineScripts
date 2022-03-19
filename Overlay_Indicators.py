f0038d1c487fb49043f1d3cea02b687b7bf09b2c
AZer <48333539+azerhouani@users.noreply.github.com>
2022-03-19 16:35:11 -0400

    Update Overlay_Indicators.py
    Updates were made to variable names, colors used, and script structure.

--- Overlay_Indicators.py
+++ Overlay_Indicators.py
@@ -2,10 +2,10 @@
 //@version=5
 ////////////////////////////////////////////////////////////////
 // Author: A.Zerhouani
-// Inspired from : TradingView, @juanmirocks, Belkhayate, drsweets, PatekCharts1, dysrupt
+// Inspired from : TradingView, juanmirocks, Belkhayate, drsweets, PatekCharts1, dysrupt
 // Source: https://github.com/azerhouani/TradingView_PineScripts
-// Updated: 2022/03/14
-// Version: 9.0
+// Updated: 2022/03/17
+// Version: 12.0
 ////////////////////////////////////////////////////////////////
 
 indicator(title="Pivot lines + 4 EMA + EmaTrend + Bollinger bands + VWAP 4 lines + VWAP bands + Ichimoku Cloud by AZer", shorttitle="PP+4EMA+EmaTrend+VWAP4L+VWAPB+Ichimoku_Azer", overlay=true)
@@ -20,7 +20,7 @@ indicator(title="Pivot lines + 4 EMA + EmaTrend + Bollinger bands + VWAP 4 lines
 //      Weekly (W) if within daily chart
 //      Monthly (M) if within weekly chart
 //      3 Months (3M) if within monthly chart
-// # Advantages over TV's indicator "Pivot Points Standard"
+// Advantages over TV's indicator "Pivot Points Standard"
 //      1. Show pivot lines for all history, which lets you gauge trading strategies throughout time
 //      2. More sensible Default/Auto time frame; e.g. on intradays only and always the market values of yesterday's Day are used
 //      3. The halves between the key levels are also shown, which it's useful for some trading strategies
@@ -40,6 +40,7 @@ chosenTimeFrame = (inputTimeFrame == "Default") ? defaultTimeFrame : inputTimeFr
 
 getSeries(e) => request.security(syminfo.tickerid, chosenTimeFrame, e, lookahead=barmerge.lookahead_on)
 
+// Calculation section
 H = getSeries(high[1])
 L = getSeries(low[1])
 C = getSeries(close[1])
@@ -70,15 +71,17 @@ S1_5 = (S1 + S2) / 2 //aka S1.5
 S2_5 = (S2 + S3) / 2 //aka S2.5
 
 
-// UX Input Arguments
-pColor = #003CFF //color.blue
-ycColor = #FFDB00 //color.yellow
+// Style section
+pColor = #0053ff // color.blue
+ycColor = #fcff00 // color.yellow
+pivotLinesTransp = 50 //Pivot lines transparency
+fillPivotClosingTransp = 90 //Transparency between Pivot and Yesterday's closing
 
-rMainLevelColor = #FF1800 //color.red
-rHalfLevelColor = color.new(#FF1800, 70) //color.red
+rMainLevelColor = color.new(#FF1800, pivotLinesTransp) //color.red
+rHalfLevelColor = color.new(#FF1800, pivotLinesTransp + 10) //color.red
 
-sMainLevelColor = #00AE47 //color.green
-sHalfLevelColor = color.new(#00AE47, 70) //color.green
+sMainLevelColor = color.new(#00AE47, pivotLinesTransp) //color.green
+sHalfLevelColor = color.new(#00AE47, pivotLinesTransp + 10) //color.green
 
 widthOfMainLevels = 1
 widthOfHalfLevels = 1
@@ -86,13 +89,15 @@ widthOfHalfLevels = 1
 lineStyle = plot.style_circles
 pivotStyle = plot.style_linebr
 
+
+// Plot section
 plot(showPivotLines ? R3 : na, title="R3", color=rMainLevelColor, linewidth=widthOfMainLevels, style=lineStyle)
 plot(showPivotLines ? R2 : na, title="R2", color=rMainLevelColor, linewidth=widthOfMainLevels, style=lineStyle)
 plot(showPivotLines ? R1 : na, title="R1", color=rMainLevelColor, linewidth=widthOfMainLevels, style=lineStyle)
 
 pivotPlot = plot(showPivotLines ? P : na, title="Pivot", color=pColor, linewidth=widthOfMainLevels, style=pivotStyle)
 closePlot = plot(showPivotLines ? YC : na, title="Yesterday's closing", color=ycColor, linewidth=widthOfMainLevels, style=pivotStyle)
-fill(pivotPlot, closePlot, YC > P ? color.new(color.green, 80) : color.new(color.red, 80), title="Daily Trend Background (Pivot based)")
+fill(pivotPlot, closePlot, YC > P ? color.new(color.green, fillPivotClosingTransp) : color.new(color.red, fillPivotClosingTransp), title="Daily Trend Background (Pivot based)")
 
 plot(showPivotLines ? S1 : na, title="S1", color=sMainLevelColor, linewidth=widthOfMainLevels, style=lineStyle)
 plot(showPivotLines ? S2 : na, title="S2", color=sMainLevelColor, linewidth=widthOfMainLevels, style=lineStyle)
@@ -117,20 +122,30 @@ g4EmaTrend = "[02]========[ 4 EMA and EMA Trend ]====================="
 // Show section
 show4EmaLines = input.bool(true, title="Show 4 EMA lines ?", group=g4EmaTrend)
 
+// Input section
 ema1 = input(20, title="EMA1 length", group=g4EmaTrend)
 ema2 = input(50, title="EMA2 length", group=g4EmaTrend)
 ema3 = input(100, title="EMA3 length", group=g4EmaTrend)
 ema4 = input(200, title="EMA4 length", group=g4EmaTrend)
 
+// Calculation section
 positifTrendLine = ta.ema(close, ema1)
 negatifTrendLine = ta.ema(close, ema2)
 
-ema1Plot = plot(show4EmaLines ? ta.ema(close, ema1) : na, title="EMA1 Line", color=#66ff6c, linewidth=1)
-ema2Plot = plot(show4EmaLines ? ta.ema(close, ema2) : na, title="EMA2 Line",color=#ff2334, linewidth=1)
-fill(ema1Plot, ema2Plot, positifTrendLine > negatifTrendLine ? color.new(color.green, 90) : color.new(color.red, 90), title="Trend Background (EMA 1,2 based)")
+// Style section
+emaLinesTransparency = 60
+fillEma1Ema2Transp = 95
+ema1Color = color.new(#66ff6c, emaLinesTransparency) // color.green
+ema2Color = color.new(#ff2334, emaLinesTransparency) // color.red
+ema3and4Color = color.new(#ffee73, emaLinesTransparency) // color.yellow
 
-plot(show4EmaLines ? ta.ema(close, ema3) : na, title="EMA3 Line", color=#f2c60d, linewidth=1)
-plot(show4EmaLines ? ta.ema(close, ema4) : na, title="EMA4 Line", color=#fbae04, linewidth=2)
+// Plot section
+ema1Plot = plot(show4EmaLines ? ta.ema(close, ema1) : na, title="EMA1 Line", color=ema1Color, linewidth=1)
+ema2Plot = plot(show4EmaLines ? ta.ema(close, ema2) : na, title="EMA2 Line",color=ema2Color, linewidth=1)
+fill(ema1Plot, ema2Plot, positifTrendLine > negatifTrendLine ? color.new(color.green, fillEma1Ema2Transp) : color.new(color.red, fillEma1Ema2Transp), title="Trend Background (EMA 1,2 based)")
+
+plot(show4EmaLines ? ta.ema(close, ema3) : na, title="EMA3 Line", color=ema3and4Color, linewidth=1)
+plot(show4EmaLines ? ta.ema(close, ema4) : na, title="EMA4 Line", color=ema3and4Color, linewidth=2)
 
 
 /////////////////////////////////////////////////////
@@ -143,16 +158,20 @@ gBollingerBands = "[03]========[ Bollinger bands ]========================="
 // Show section
 showBollingerB = input.bool(true, title="Show Bollinger bands ?", group=gBollingerBands)
 
+// Input section
 bbLength = input.int(20, title="BB Length", minval=1, group=gBollingerBands)
 bbSrc = input(close, title="BB Source", group=gBollingerBands)
 bbMult = input.float(2.0, title="BB StdDev", minval=0.001, maxval=50, group=gBollingerBands)
 bbOffset = input.int(0, title="BB Offset", minval=-500, maxval=500, group=gBollingerBands)
 
+// Calculation section
 [bbBasis, bbUpper, bbLower] = ta.bb(bbSrc, bbLength, bbMult)
-plot(showBollingerB ? bbBasis : na, title="BB SMA", color=#ff9800, offset=bbOffset)
-bbUpperPlot = plot(showBollingerB ? bbUpper : na, title="BB Upper line", color=#e91e63, linewidth=2, offset=bbOffset)
-bbLowerPlot = plot(showBollingerB ? bbLower : na, title="BB Lower line", color=#4caf50, linewidth=2, offset=bbOffset)
-fill(bbUpperPlot, bbLowerPlot, color=color.new(#2196f3, 90), title="BB Background")
+
+// Plot section
+plot(showBollingerB ? bbBasis : na, title="BB SMA", color=#ff6d00, offset=bbOffset)
+bbUpperPlot = plot(showBollingerB ? bbUpper : na, title="BB Upper line", color=#2196f3, linewidth=1, offset=bbOffset)
+bbLowerPlot = plot(showBollingerB ? bbLower : na, title="BB Lower line", color=#2196f3, linewidth=1, offset=bbOffset)
+fill(bbUpperPlot, bbLowerPlot, color=color.new(#2196f3, 95), title="BB Background")
 
 
 //////////////////////////////////////////////////////
@@ -165,37 +184,44 @@ gVwapLines  = "[04]========[ VWAP lines ]=============================="
 // Show section
 showVwapLines = input.bool(true, title="Show VWAP lines ?", group=gVwapLines)
 
+// Calculation section
 typicalPrice = (high + low + close) / 3
 typicalPriceVolume = typicalPrice * volume
 
-vwapLinesStyle = plot.style_cross
+// Style section
+vwapLine1Color = #d2b4de //color.purple and Transp=75
+vwapLine2Color = #bb8fce //color.purple and Transp=70
+vwapLine3Color = #a569bd //color.purple and Transp=65
+vwapLine4Color = #8e44ad //color.purple and Transp=60
+
+vwapLinesStyle = plot.style_cross // plot.style_linebr
 
 cumulativePeriod1 = input(48, "VWAP Period1", group=gVwapLines)
 cumulativeTypicalPriceVolume1 = math.sum(typicalPriceVolume, cumulativePeriod1)
 cumulativeVolume1 = math.sum(volume, cumulativePeriod1)
 vwapValue1 = cumulativeTypicalPriceVolume1 / cumulativeVolume1
-plot(showVwapLines ? vwapValue1 : na, color=color.new(#b2b5be, 10), style=vwapLinesStyle)
+plot(showVwapLines ? vwapValue1 : na, color=vwapLine1Color, style=vwapLinesStyle)
 
 
 cumulativePeriod2 = input(96, "VWAP Period2", group=gVwapLines)
 cumulativeTypicalPriceVolume2 = math.sum(typicalPriceVolume, cumulativePeriod2)
 cumulativeVolume2 = math.sum(volume, cumulativePeriod2)
 vwapValue2 = cumulativeTypicalPriceVolume2 / cumulativeVolume2
-plot(showVwapLines ? vwapValue2 : na, color=color.new(color.white, 10), style=vwapLinesStyle,linewidth=1)
+plot(showVwapLines ? vwapValue2 : na, color=vwapLine2Color, style=vwapLinesStyle,linewidth=1)
 
 
 cumulativePeriod3 = input(196, "VWAP Period3", group=gVwapLines)
 cumulativeTypicalPriceVolume3 = math.sum(typicalPriceVolume, cumulativePeriod3)
 cumulativeVolume3 = math.sum(volume, cumulativePeriod3)
 vwapValue3 = cumulativeTypicalPriceVolume3 / cumulativeVolume3
-plot(showVwapLines ? vwapValue3 : na, color=color.new(#ffe0b2, 10), style=vwapLinesStyle, linewidth=1)
+plot(showVwapLines ? vwapValue3 : na, color=vwapLine3Color, style=vwapLinesStyle, linewidth=1)
 
 
 cumulativePeriod4 = input(484, "VWAP Period4", group=gVwapLines)
 cumulativeTypicalPriceVolume4 = math.sum(typicalPriceVolume, cumulativePeriod4)
 cumulativeVolume4 = math.sum(volume, cumulativePeriod4)
 vwapValue4 = cumulativeTypicalPriceVolume4 / cumulativeVolume4
-plot(showVwapLines ? vwapValue4 : na, color=color.new(#cfb9ff, 10), style=vwapLinesStyle, linewidth=1)
+plot(showVwapLines ? vwapValue4 : na, color=vwapLine4Color, style=vwapLinesStyle, linewidth=1)
 
 
 ///////////////////////////////////////////////////
@@ -274,20 +300,23 @@ dev5 = 4.01*stdevMult2
 std = vwapStdev()
 
 // DEV PLOTS
+vwapUpperBandColor = color.new(color.red, 80)
+vwapLowerBandColor = color.new(color.green, 80)
 
-U5 = plot(showBands ? myvwap + std * dev5 : na, title="Upper Band 5", color=color.new(color.red, 75), offset=vwapOffset)
-U4 = plot(showBands ? myvwap + std * dev4 : na, title="Upper Band 4", color=color.new(color.red, 75), offset=vwapOffset)
-U3 = plot(showBands ? myvwap + std * dev3 : na, title="Upper Band 3", color=color.new(color.red, 75), offset=vwapOffset)
-U2 = plot(showBands ? myvwap + std * dev2 : na, title="Upper Band 2", color=color.new(color.red, 75), offset=vwapOffset)
-U1 = plot(showBands ? myvwap + std * dev1 : na, title="Upper Band 1", color=color.new(color.red, 75), offset=vwapOffset)
+
+U5 = plot(showBands ? myvwap + std * dev5 : na, title="Upper Band 5", color=vwapUpperBandColor, offset=vwapOffset)
+U4 = plot(showBands ? myvwap + std * dev4 : na, title="Upper Band 4", color=vwapUpperBandColor, offset=vwapOffset)
+U3 = plot(showBands ? myvwap + std * dev3 : na, title="Upper Band 3", color=vwapUpperBandColor, offset=vwapOffset)
+U2 = plot(showBands ? myvwap + std * dev2 : na, title="Upper Band 2", color=vwapUpperBandColor, offset=vwapOffset)
+U1 = plot(showBands ? myvwap + std * dev1 : na, title="Upper Band 1", color=vwapUpperBandColor, offset=vwapOffset)
 
 plot(showBands ? myvwap : na, title="VWAP", offset=vwapOffset, color=color.white)
 
-D1 = plot(showBands ? myvwap - std * dev1 : na, title="Lower Band 1", color=color.new(color.green, 75), offset=vwapOffset)
-D2 = plot(showBands ? myvwap - std * dev2 : na, title="Lower Band 2", color=color.new(color.green, 75), offset=vwapOffset)
-D3 = plot(showBands ? myvwap - std * dev3 : na, title="Lower Band 3", color=color.new(color.green, 75), offset=vwapOffset)
-D4 = plot(showBands ? myvwap - std * dev4 : na, title="Lower Band 4", color=color.new(color.green, 75), offset=vwapOffset)
-D5 = plot(showBands ? myvwap - std * dev5 : na, title="Lower Band 5", color=color.new(color.green, 75), offset=vwapOffset)
+D1 = plot(showBands ? myvwap - std * dev1 : na, title="Lower Band 1", color=vwapLowerBandColor, offset=vwapOffset)
+D2 = plot(showBands ? myvwap - std * dev2 : na, title="Lower Band 2", color=vwapLowerBandColor, offset=vwapOffset)
+D3 = plot(showBands ? myvwap - std * dev3 : na, title="Lower Band 3", color=vwapLowerBandColor, offset=vwapOffset)
+D4 = plot(showBands ? myvwap - std * dev4 : na, title="Lower Band 4", color=vwapLowerBandColor, offset=vwapOffset)
+D5 = plot(showBands ? myvwap - std * dev5 : na, title="Lower Band 5", color=vwapLowerBandColor, offset=vwapOffset)
 
 // FILL CONDITIONS
 
@@ -333,16 +362,16 @@ DVUP4 = ta.crossover(ma, U44)
 
 // ALERT CONDITIONS
 
-alertcondition(VW, title='Vwap', message='Vwap')
-alertcondition(DV1, title='Low Deviation 1', message='Vwap Low Deviation 1')
-alertcondition(DV2, title='Low Deviation 2', message='Vwap Low Deviation 2')
-alertcondition(DV3, title='Low Deviation 3', message='Vwap Low Deviation 3')
-alertcondition(DV4, title='Low Deviation 4', message='Vwap Low Deviation 4')
-
-alertcondition(DVUP1, title='High Deviation 1', message='Vwap High Deviation 1')
-alertcondition(DVUP2, title='High Deviation 2', message='Vwap High Deviation 2')
-alertcondition(DVUP3, title='High Deviation 3', message='Vwap High Deviation 3')
-alertcondition(DVUP4, title='High Deviation 4', message='Vwap High Deviation 4')
+//alertcondition(VW, title='Vwap', message='Vwap')
+//alertcondition(DV1, title='Low Deviation 1', message='Vwap Low Deviation 1')
+//alertcondition(DV2, title='Low Deviation 2', message='Vwap Low Deviation 2')
+//alertcondition(DV3, title='Low Deviation 3', message='Vwap Low Deviation 3')
+//alertcondition(DV4, title='Low Deviation 4', message='Vwap Low Deviation 4')
+//
+//alertcondition(DVUP1, title='High Deviation 1', message='Vwap High Deviation 1')
+//alertcondition(DVUP2, title='High Deviation 2', message='Vwap High Deviation 2')
+//alertcondition(DVUP3, title='High Deviation 3', message='Vwap High Deviation 3')
+//alertcondition(DVUP4, title='High Deviation 4', message='Vwap High Deviation 4')
 
 // SIGNALS
 
@@ -369,24 +398,23 @@ gIchimokuCloud  = "[06]========[ Ichimoku cloud ]=========================="
 // Show section
 showIchimokuCloud = input(true, title="Show Ichimoku cloud ?", group=gIchimokuCloud)
 
-
+// Input section
 ichiConversionPeriods = input.int(9, minval=1, title="Conversion Line Length", group=gIchimokuCloud)
 ichiBasePeriods = input.int(26, minval=1, title="Base Line Length", group=gIchimokuCloud)
 ichiLaggingSpan2Periods = input.int(52, minval=1, title="Leading Span B Length", group=gIchimokuCloud)
 ichiDisplacement = input.int(26, minval=1, title="Displacement", group=gIchimokuCloud)
 
-
+// Calculation section
 donchian(len) => math.avg(ta.lowest(len), ta.highest(len))
 conversionLine = donchian(ichiConversionPeriods)
 baseLine = donchian(ichiBasePeriods)
 leadLine1 = math.avg(conversionLine, baseLine)
 leadLine2 = donchian(ichiLaggingSpan2Periods)
 
-
+// Plot section
 plot(showIchimokuCloud ? conversionLine : na, color=#2962FF, title="Conversion Line")
 plot(showIchimokuCloud ? baseLine : na, color=#B71C1C, title="Base Line")
 plot(showIchimokuCloud ? close : na, offset = -ichiDisplacement + 1, color=#43A047, title="Lagging Span")
 p1 = plot(showIchimokuCloud ? leadLine1 : na, offset = ichiDisplacement - 1, color=#A5D6A7, title="Leading Span A")
 p2 = plot(showIchimokuCloud ? leadLine2 : na, offset = ichiDisplacement - 1, color=#EF9A9A, title="Leading Span B")
 fill(p1, p2, color = leadLine1 > leadLine2 ? color.rgb(67, 160, 71, 90) : color.rgb(244, 67, 54, 90))
-
